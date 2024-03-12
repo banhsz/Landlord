669656c1924d3362d134dc35ccb3e2184a50cb2e
banhsz <kozikbro@gmail.com>
2024-03-12 21:14:13 +0100

    chart max height, fix for uw screens

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -51,16 +51,16 @@ $this->registerJsFile('/js/chart/index/indexCharts.js', ['position' => View::POS
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6 col-xl-4 col-xxl-3 p-4">
+            <div class="col-sm-6 col-xl-4 col-xxl-3 p-4 max-h-450">
                 <canvas id="invoiceChart" data-paid="<?= $paidInvoices ?? 0 ?>" data-unpaid="<?= $unpaidInvoices ?? 0 ?>"></canvas>
             </div>
-            <div class="col-sm-6 col-xl-4 col-xxl-3 p-4">
+            <div class="col-sm-6 col-xl-4 col-xxl-3 p-4 max-h-450">
                 <canvas id="apartmentChart" data-ocucupied="<?= $occupiedApartmentCount ?? 0 ?>" data-free="<?= ($apartmentCount ?? 0) - ($occupiedApartmentCount ?? 0)?>"></canvas>
             </div>
-            <div class="col-sm-6 col-xl-4 col-xxl-3 p-4">
+            <div class="col-sm-6 col-xl-4 col-xxl-3 p-4 max-h-450">
                 <canvas id="rentalChart" data-active="<?= $activeRentalCount ?? 0 ?>" data-future="<?= $futureRentalCount ?? 0 ?>" data-expired="<?= $expiredRentalCount ?? 0 ?>"></canvas>
             </div>
-            <div class="col-sm-6 col-xl-4 col-xxl-3 p-4">
+            <div class="col-sm-6 col-xl-4 col-xxl-3 p-4 max-h-450">
                 <canvas id="tenantChart" data-tenant="<?= $tenantCount ?? 0 ?>"> </canvas>
             </div>
         </div>
