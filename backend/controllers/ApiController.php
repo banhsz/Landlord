8de15372857dd7a842ea950ea74b8a633d85f8d4
banhsz <kozikbro@gmail.com>
2024-11-26 21:49:24 +0100

    #4 fixed default application auth config. created test api endpoints with user aut_key authentication

--- backend/controllers/ApiController.php
+++ backend/controllers/ApiController.php
@@ -2,42 +2,123 @@
 
 namespace backend\controllers;
 
-use backend\models\Apartment;
+use backend\models\Invoice;
+use backend\models\Notification;
+use common\models\User;
+use Yii;
 use yii\filters\VerbFilter;
+use yii\web\BadRequestHttpException;
 use yii\web\Controller;
 use yii\web\Response;
 
 class ApiController extends Controller
 {
-    public $enableCsrfValidation = false; // Disable CSRF validation for API
+    public User $user;
+    public $enableCsrfValidation = false;
 
-    public function behaviors()
-    {
+    public function behaviors() {
+        $behaviors = parent::behaviors();
+        $behaviors['verbs'] = [
+            'class'     => VerbFilter::class,
+            'actions'   => [ // List actions and their allowed http methods here
+                'test'              => ['get', 'post', 'put'],
+                'get-invoices'      => ['get'],
+                'post-notification' => ['post']
+            ],
+        ];
+        return $behaviors;
+    }
+
+    /**
+     * Overrides base class beforeAction function. This will
+     * force token based auth for all actions within this controller.
+     *
+     * Note: pass User 'auth_key' in header with name 'auth'
+     *
+     * @param $action
+     * @return bool
+     * @throws BadRequestHttpException
+     */
+    public function beforeAction($action) {
+        Yii::$app->response->format = Response::FORMAT_JSON;
+
+        // Find user based on passed auth token
+        $token = Yii::$app->request->getHeaders()->get('auth');
+        if ($token) {
+            if ($potentialUser = User::findOne(['auth_key' => $token])) {
+                $this->user = $potentialUser;
+                return parent::beforeAction($action);
+            }
+        }
+
+        // If no user was found
+        Yii::$app->response->statusCode = 403;
+        Yii::$app->response->data = [
+            'result'    => 'error',
+            'data'      => 'Access denied!'
+        ];
+        return false;
+    }
+
+
+    /**
+     * GET POST PUT api/test
+     *
+     * @api
+     * @return string[]
+     */
+    public function actionTest() {
         return [
-            'contentNegotiator' => [
-                'class' => 'yii\filters\ContentNegotiator',
-                'only' => ['your-action'],
-                'formats' => [
-                    'application/json' => Response::FORMAT_JSON,
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::class,
-                'actions' => [
-                    'your-action' => ['get'], // Specify that 'your-action' only accepts GET requests
-                ],
-            ],
-            'corsFilter' => [
-                'class' => \yii\filters\Cors::class,
-            ],
+            'result'    => 'Success',
+            'data'      => 'It works.'
         ];
     }
 
-    public function actionYourAction()
-    {
-        //$response = ['message' => 'This is a modelless API endpoint'];
-        //return $response;
-        $apartments = Apartment::find()->asArray()->all();
-        return $apartments;
+    /**
+     * GET api/get-invoices
+     *
+     * @api
+     * @return array
+     */
+    public function actionGetInvoices() {
+        return [
+            'result'    => 'Success',
+            'data'      => Invoice::find()->asArray()->all()
+       ];
+    }
+
+    /**
+     * POST api/post-notification
+     * {
+     *  "message": "test notification 3"
+     * }
+     *
+     * @api
+     * @return string[]
+     */
+    public function actionPostNotification() {
+        // request body
+        $data = Yii::$app->request->getRawBody();
+        $dataArray = json_decode($data, true);
+        if (!isset($dataArray["message"])) {
+            Yii::$app->response->statusCode = 400;
+            return [
+                'result'    => 'Error',
+                'data'      => "Bad request. Key 'message' is required!"
+            ];
+        }
+
+        if (Notification::createNotification($dataArray["message"])) {
+            return [
+                'result' => 'Success',
+            ];
+        } else {
+            Yii::$app->response->statusCode = 500;
+            return [
+                'result'    => 'Error',
+                'data'      => 'Failed to create notification.',
+            ];
+        }
+
     }
 }
